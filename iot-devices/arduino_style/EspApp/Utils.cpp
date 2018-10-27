77805a737d8b215517cec374c16656ca43badda6
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-10-28 00:28:32 +0530

    EspApp arduino: Generate unique device id from chidid and flash id

--- iot-devices/arduino_style/EspApp/Utils.cpp
+++ iot-devices/arduino_style/EspApp/Utils.cpp
@@ -3,9 +3,11 @@
 #include "Config.h"
 
 String Utils::genDeviceId(){
-  String dev_id = "ESP8266C-";   // Create a random MQTT client ID
-  dev_id += String(random(0xffff), HEX);
+  String chipid = String(ESP.getChipId(),HEX);
+  String flashId = String(ESP.getFlashChipId(),HEX);
+  String deviceId = String("ESP" + chipid + flashId);
+  deviceId.toUpperCase();
   Serial.print("Device id generated : ");
-  Serial.println(dev_id);
-  return dev_id;
+  Serial.println(deviceId);
+  return deviceId;
 }
